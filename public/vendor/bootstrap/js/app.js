5fa949c6ead4967bf1af2a198b5c5e6793cf30d0
GED <eduardgimaev@Eduards-MacBook-Air.local>
2023-05-15 15:48:07 +0500

    add function for counting products in the basket

--- public/vendor/bootstrap/js/app.js
+++ public/vendor/bootstrap/js/app.js
@@ -17,3 +17,17 @@ function add_to_card(id)
     window.localStorage.setItem(key, x);
 }
 
+function basket()  
+{
+    var cnt = 0;
+    for (var i = 0; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i);
+        var value = window.localStorage.getItem(key); // analoig in Ruby: hh[key] = x
+        if(key.indexOf('product') == 0)
+        {
+            cnt=cnt+value*1;
+        }
+    }
+    return cnt;
+}
